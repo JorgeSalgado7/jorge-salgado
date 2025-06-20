2bfe464f33ae314d6c08f9fc144336f5514297a9
JorgeSalgado7 <j.salgado7@outlook.com>
2025-06-20 08:42:56 -0600

    Feature: Footer

--- js/template.js
+++
@@ -1,7 +0,0 @@
-const url = window.location.origin 
-
-fetch(`${url}/layout/footer.html`)
-.then(response => response.text())
-.then(data => {
-	document.getElementById('footer').innerHTML = data
-})
