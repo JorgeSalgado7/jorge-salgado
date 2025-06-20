b32f7d9caa5d91ee9fa7139e3b88c75829bca584
JorgeSalgado7 <j.salgado7@outlook.com>
2025-06-19 22:27:53 -0600

    Feature: Template

--- js/template.js
+++ js/template.js
@@ -1,4 +1,6 @@
-fetch('layout/footer.html')
+const url = window.location.origin 
+
+fetch(`${url}/layout/footer.html`)
 .then(response => response.text())
 .then(data => {
 	document.getElementById('footer').innerHTML = data
